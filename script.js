550d0a4887a8d59373f5f08734f9645859be0b6e
Paula Burghelea <paulaburghelea.github@gmail.com>
2022-05-16 10:50:16 +0100

    -added

---
+++ script.js
@@ -0,0 +1,33 @@
+// console.log(sel[1]);
+const url = "https://api.sampleapis.com/coffee/hot";
+
+addEventListener("load", async () => {
+  try {
+    const response = await fetch(url);
+    console.log("response:", response);
+
+    if (!response.ok) throw response;
+
+    const data = await response.json();
+    console.log("data:", data);
+    const sel = document.getElementById("coffeeList");
+    const frag = document.createDocumentFragment();
+
+    // ADDED DEFAULT OPTION
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.textContent = " --Please choose-- ";
+    frag.append(defaultOption);
+
+    for (const drink of data) {
+      const option = document.createElement("option");
+      option.value = drink.id;
+      option.textContent = drink.title;
+      console.log(option);
+      frag.append(option);
+    }
+    sel.replaceChildren(frag);
+  } catch (err) {
+    console.log("err", err);
+  }
+});
